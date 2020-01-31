580aafdef5d49a4f9bec9a46cc4607aead1dec71
wicktem <artemrepetsky3107@gmail.com>
2020-01-31 16:22:43 +0300

    UN1-T125

--- admin/include/functions/post-type-customers.php
+++ admin/include/functions/post-type-customers.php
@@ -8,7 +8,7 @@ function unid_load_admin_scripts( $hook ) {
 	// If the post we're editing isn't a project_summary type, exit this function.
 	// var_dump(get_post_type());
 	if ( 'customers' === get_post_type() && ($hook == 'post-new.php' || $hook == 'post.php') ) {
-		wp_enqueue_script( 'admin_scripts',  plugins_url(MY_PLUGIN_NAME.'/admin/js/functions/post-type-customers.js'), array( 'jquery' ), false );
+		wp_enqueue_script( 'admin_scripts',  plugins_url(MY_PLUGIN_NAME.'/admin/js/functions/valida-form.js'), array( 'jquery' ), false );
 	}
 }
 
