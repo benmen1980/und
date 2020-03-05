9d4e71c6ac955e17c1aed6e273ac51c18e9cc69a
Kaushik <kk@narola.email>
2020-03-05 15:50:42 +0530

    UN2-T10
    Shipping price per campaign

--- admin/meta-boxes/meta-box-shipping.php
+++ admin/meta-boxes/meta-box-shipping.php
@@ -1,7 +1,7 @@
 <?php
 
-add_action( 'admin_menu', 'add_shipping_option' );
-add_action( 'save_post',  'save_shipping_option');
+add_action( 'admin_menu', 'add_shipping_option');
+add_action( 'save_post', 'save_shipping_option');
 
 function add_shipping_option() {
 	add_meta_box('campaign_shipping_option', __( 'Shipping', 'unidress' ), 'unidress_shipping_option', array('project', 'campaign'), 'normal');
@@ -10,13 +10,12 @@ function add_shipping_option() {
 function unidress_shipping_option($post) {
 	wp_enqueue_script( 'shipping-js', plugins_url( '/unidress/admin/js/meta-box-shipping.js'), array( 'jquery' ) );
 
-    $post_type = $post->post_type;
+	$post_type = $post->post_type;
 
 	$customer_id = '';
 	if ($post_type == 'project') {
 		$customer_id = get_post_meta($post->ID, 'project_customer', true);
-	}
-    elseif ($post_type == 'campaign') {
+	} elseif ($post_type == 'campaign') {
 		$customer_id = get_post_meta($post->ID, 'campaign_customer', true);
 	}
 	echo render_unidress_shops_shipping_option($customer_id, $post->ID);
@@ -28,19 +27,36 @@ function unidress_shipping_option($post) {
 
 function render_shipping_to_user_brunch($campaign_id) {
 	get_post_meta($campaign_id, 'shipping_allow', true) ? $checked = 'checked="checked"' : $checked='';
+	$min_order_value = get_post_meta($campaign_id, 'min_order_value', true) ?: 0;
+	$shipping_price = get_post_meta($campaign_id, 'shipping_price', true) ?: 0;
 
-	$output = '<fieldset>';
-	$output .= '<h4 class="shipping-option-label">' . esc_html__( 'Shipping to user\'s brunch', 'unidress' ) .'</h4>';
-	$output .= '<ul class="acf-checkbox-list acf-bl">';
-	$output .= '	<li>';
-	$output .= '		<label>';
-	$output .= '			<input type="checkbox" name="shipping_allow" ' .$checked. '>';
-	$output .=              __( 'Allow shipping to user\'s brunch', 'unidress' );
-	$output .= '		</label>';
-	$output .= '	</li>';
-	$output .= '</ul>';
-	$output .= '</fieldset>';
-
+	$output = '<fieldset>
+				<table>
+					<thead>
+						<tr>
+							<th><h4 class="shipping-option-label">' . esc_html__( 'Shipping to user\'s brunch', 'unidress' ) .'</h4></th>
+							<th><h4 class="shipping-option-label">' . esc_html__( 'Minimum order value', 'unidress' ) .'</h4></th>
+							<th><h4 class="shipping-option-label">' . esc_html__( 'Shipping Price', 'unidress' ) .'</h4></th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>
+								<ul class="acf-checkbox-list acf-bl">
+									<li>
+										<label>
+											<input type="checkbox" name="shipping_allow" ' .$checked. '>
+											'.__( 'Allow shipping to user\'s brunch', 'unidress' ).'
+										</label>
+									</li>
+								</ul>
+							</td>
+							<td><input type="number" name="min_order_value" value="'.$min_order_value.'"></td>
+							<td><input type="number" name="shipping_price" value="'.$shipping_price.'"></td>
+						</tr>
+					</tbody>
+				</table>
+			</fieldset>';
 	return $output;
 
 }
@@ -59,10 +75,10 @@ function render_unidress_shops_shipping_option($customer_id = '', $campaign_id) 
 
 	$output .= '<h4 class="shipping-option-label">' . esc_html__( 'Unidress shops', 'unidress' ) .'</h4>';
 
-	$output .= '		<label>';
-	$output .= '			<input name="shops[]" class="shipping-all-select" type="checkbox" >';
-	$output .=              esc_html__( 'Select all', 'unidress' );
-	$output .= '		</label>';
+	$output .= '<label>
+					<input name="shops[]" class="shipping-all-select" type="checkbox">
+					'.esc_html__( 'Select all', 'unidress' ).'
+				</label>';
 
 	if ($shops) {
 		$output .= '<ul class="acf-checkbox-list acf-bl">';
@@ -77,7 +93,7 @@ function render_unidress_shops_shipping_option($customer_id = '', $campaign_id) 
 			$output .= '	<li>';
 			$output .= '		<label>';
 			$output .= '			<input type="checkbox" class="shipping-select" name="shops[]" value="' . $shop->ID . '" ' .$checked. '>';
-			$output .=              $shop->post_title;
+			$output .=				$shop->post_title;
 			$output .= '		</label>';
 			$output .= '	</li>';
 		}
@@ -117,10 +133,9 @@ function render_customer_branches_address_shipping_option($customer_id = '', $ca
 
 		$output = '<fieldset class="unidress-shops-shipping">';
 		$output .= '<h4 class="shipping-option-label">' . esc_html__( 'Branches addresses', 'unidress' ) .'</h4>';
-
 		$output .= '		<label>';
 		$output .= '			<input name="shops[]" class="shipping-all-select" type="checkbox" >';
-		$output .=              esc_html__( 'Select all', 'unidress' );
+		$output .=				esc_html__( 'Select all', 'unidress' );
 		$output .= '		</label>';
 		$output .= '<ul class="acf-checkbox-list acf-bl">';
 		foreach( $shops_branch as $shop ){
@@ -130,11 +145,10 @@ function render_customer_branches_address_shipping_option($customer_id = '', $ca
 			}
 			setup_postdata($shop);
 
-			
 			$output .= '	<li>';
 			$output .= '		<label>';
 			$output .= '			<input type="checkbox" name="shops[]" class="shipping-select" value="' . $shop->ID . '" ' .$checked. '>';
-			$output .=              $shop->post_title;
+			$output .=				$shop->post_title;
 			$output .= '		</label>';
 			$output .= '	</li>';
 			
@@ -153,8 +167,7 @@ function render_customer_branches_address_shipping_option($customer_id = '', $ca
 
 function save_shipping_option( $post_id ) {
 
-	if ( !isset( $_POST['project_shipping_option'] )
-	     || !wp_verify_nonce( $_POST['project_shipping_option'], basename( __FILE__ ) ) )
+	if ( !isset( $_POST['project_shipping_option'] ) || !wp_verify_nonce( $_POST['project_shipping_option'], basename( __FILE__ ) ) )
 		return $post_id;
 
 	if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE )
@@ -163,22 +176,25 @@ function save_shipping_option( $post_id ) {
 	if ( !current_user_can( 'edit_post', $post_id ) )
 		return $post_id;
 
-    $shops = isset($_POST['shops']) ? $_POST['shops'] : '';
+	$shops = isset($_POST['shops']) ? $_POST['shops'] : '';
 	$shipping_allow = isset($_POST['shipping_allow']) ? $_POST['shipping_allow'] : '';
+	$min_order_value = isset($_POST['min_order_value']) ? $_POST['min_order_value'] : 0;
+	$shipping_price = isset($_POST['shipping_price']) ? $_POST['shipping_price'] : 0;
 	update_post_meta($post_id, 'shops', $shops);
 	update_post_meta($post_id, 'shipping_allow', $shipping_allow);
-
+	update_post_meta($post_id, 'min_order_value', $min_order_value);
+	update_post_meta($post_id, 'shipping_price', $shipping_price);
 }
 
 //Show shipping in checkout
-add_action( 'unidress_shipping_select',  'show_shipping_option_in_checkout');
+add_action( 'unidress_shipping_select', 'show_shipping_option_in_checkout');
 function show_shipping_option_in_checkout() {
 
-    if (!is_user_logged_in())
-        return;
+	if (!is_user_logged_in())
+		return;
 
 	$user_id        = get_current_user_id();
-	$customer_id    = get_user_meta($user_id,     'user_customer', true);
+	$customer_id    = get_user_meta($user_id, 'user_customer', true);
 	$campaign_id    = get_post_meta($customer_id, 'active_campaign', true);
 	$shops_checked  = get_post_meta($campaign_id, 'shops', true);
 	$shipping_allow = get_post_meta($campaign_id, 'shipping_allow', true);
@@ -189,10 +205,10 @@ function show_shipping_option_in_checkout() {
 
 	$shops = get_posts( array(
 		'numberposts' => -1,
-		'include'     => $shops_checked,
-		'orderby'     => 'date',
-		'order'       => 'DESC',
-		'post_type'   => 'shop',
+		'include' => $shops_checked,
+		'orderby' => 'date',
+		'order' => 'DESC',
+		'post_type' => 'shop',
 		'suppress_filters' => true,
 	) );
 	$output = '';
@@ -206,20 +222,20 @@ function show_shipping_option_in_checkout() {
 		$output .= '<th>'. esc_html__( 'Shipping to Unidress Shop', 'unidress' ) .'</th>';
 		$output .= '<td>';
 
-        foreach( $shops as $shop ){
+		foreach( $shops as $shop ){
 
-	        $output .= '<ul class="cart-shipping-list">';
-            $output .= '	<li>';
-            $output .= '		<label>';
-            $output .= '			<input type="radio" name="unidress_shipping" value="' . $shop->ID . '">';
-            $output .=              $shop->post_title;
-            $output .= '		</label>';
-            $output .= '	</li>';
-            $output .= '</ul>';
+			$output .= '<ul class="cart-shipping-list">';
+			$output .= '	<li>';
+			$output .= '		<label>';
+			$output .= '			<input type="radio" name="unidress_shipping" value="' . $shop->ID . '">';
+			$output .=				$shop->post_title;
+			$output .= '		</label>';
+			$output .= '	</li>';
+			$output .= '</ul>';
 
-        }
+		}
 
-        $output .= '</td>';
+		$output .= '</td>';
 	}
 
 
@@ -238,7 +254,7 @@ function show_shipping_option_in_checkout() {
 		$output .= '	<li>';
 		$output .= '		<label>';
 		$output .= '			<input type="radio" name="unidress_shipping" value="' . $user_branch . '" checked="checked">';
-		$output .=              $title;
+		$output .=				$title;
 		$output .= '		</label>';
 		$output .= '	</li>';
 		$output .= '</ul>';
@@ -267,7 +283,7 @@ function show_shipping_option_in_checkout() {
 				$output .= '	<li>';
 				$output .= '		<label>';
 				$output .= '			<input type="radio" name="unidress_shipping" value="' . $shop->ID . '">';
-				$output .=              $shop->post_title;
+				$output .=				$shop->post_title;
 				$output .= '		</label>';
 				$output .= '	</li>';
 			}
@@ -284,4 +300,3 @@ function show_shipping_option_in_checkout() {
 
 	echo $output;
 }
-
